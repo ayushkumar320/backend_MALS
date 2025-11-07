0f0831edeccb2123a250ecc9438157a81c77468e
Ayush <ayushkumar85385@gmail.com>
2025-11-07 17:36:00 +0530

    updated the teacher model

--- models/teacher.js
+++ models/teacher.js
@@ -6,6 +6,10 @@ const techerSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  name: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
@@ -18,7 +22,7 @@ const techerSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  workingHourse: {
+  workingHour: {
     type: Number,
     required: true,
   },
