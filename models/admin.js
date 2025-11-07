12ada481ad6ffdd55be005fcc99bc6a8bfe55f65
mobasshir <mobasshirkhan9931@gmail.com>
2025-11-07 16:27:47 +0530

    admin schema

---
+++ models/admin.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    labCapacity: { 
+        type: Number, 
+        default: 60,
+        required: true
+    },
+    classCapacity: { 
+        type: Number, 
+        default: 60,
+        required: true
+    },
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+export default Admin;
